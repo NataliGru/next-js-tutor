242f46f7283899cac4978287a7477fdab8831520
Nataliia Hrushanyk <nataligrushanyk@gmail.com>
2026-02-06 18:15:02 +0200

    init

---
+++ nextjs-dashboard/app/layout.tsx
@@ -0,0 +1,16 @@
+import '@/app/ui/global.css';
+
+import { inter } from '@/app/ui/fonts';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      {/* the Tailwind antialiased class which smooths out the font. It's not necessary to use this class, but it adds a nice touch. */}
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
